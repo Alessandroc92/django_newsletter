a0a841a915e009729b65cd6cb692e605f0c59e6b
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-05-24 15:33:11 +0200

    Fix typo for newsletter urls.py

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('newsletter.employees.urls')),
-    path('',include('newsletter.notifications.url'))
+    path('',include('newsletter.notifications.urls'))
 ]
