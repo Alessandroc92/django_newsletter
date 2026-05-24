273321659d33098c6c4a54b2ab801f9f0e13ab38
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-05-24 14:05:57 +0200

    Create Team model to map members of teams within a given branch

--- newsletter/employees/models.py
+++ newsletter/employees/models.py
@@ -6,3 +6,24 @@ class Branch(models.Model):
     branch_id = models.AutoField(primary_key=True)
     country = models.CharField(max_length=255, null=False)
     city = models.CharField(max_length=255, null=False)
+    
+
+class Team(models.Model):
+    """A model class representing teams of a specific branch."""
+    class TeamType(models.TextChoices):
+        ADMINISTRATIVE = 'Administrative'
+        DESIGN = 'UX/UI Design'
+        SOFTWARE_DEVELOPMENT = 'Software Development'
+        HR = 'HR'
+    branch = models.ForeignKey(
+        Branch,
+        on_delete=models.DO_NOTHING,
+        null=True,
+        related_name='teams'
+    )
+    team_id = models.AutoField(primary_key=True)
+    team_type = models.CharField(
+        max_length=45,
+        choices=TeamType.choices,
+        default=TeamType.SOFTWARE_DEVELOPMENT
+        )
