2b569e34c2de6bde632d8898556510b8832e7937
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-05-24 14:00:21 +0200

    Create branch model for the multinational company

--- newsletter/employees/models.py
+++ newsletter/employees/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Branch(models.Model):
+    """A model class representing branches of the multinational company."""
+    branch_id = models.AutoField(primary_key=True)
+    country = models.CharField(max_length=255, null=False)
+    city = models.CharField(max_length=255, null=False)
