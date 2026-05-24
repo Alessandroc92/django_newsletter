5a90318362b2f94714722829188f91aa3faf35ce
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-05-24 16:13:43 +0200

    Add missing routers from employees app

--- newsletter/employees/urls.py
+++ newsletter/employees/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register('employees', viewset=views.EmployeeViewSet)
+router.register('teams', viewset=views.TeamViewSet)
+router.register('branches', viewset=views.BranchViewSet)
 urlpatterns = router.urls
