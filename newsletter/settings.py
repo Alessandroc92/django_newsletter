44a9bbb8eb352bb5064ca05a2fb409a967650f5b
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-05-24 16:15:58 +0200

    Add configurations to newsletter's settings.py to include task backend and email backend

--- newsletter/settings.py
+++ newsletter/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_tasks',
     'rest_framework',
-    'newsletter.employees'
+    'newsletter.employees',
+    'newsletter.notifications'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+TASKS = {
+    "default": 
+        {
+            "BACKEND": "django.tasks.backends.immediate.ImmediateBackend"
+            }
+        }
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
