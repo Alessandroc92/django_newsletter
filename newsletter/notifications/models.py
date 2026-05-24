945b0b6e0493207f242e21b7eaf6d25d49f96383
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-05-24 15:24:33 +0200

    Create a NotificationRun model to track the number of emails sent for each run

--- newsletter/notifications/models.py
+++ newsletter/notifications/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class NotificationRun(models.Model):
+    notification_run_id = models.AutoField(primary_key=True)
+    n_recipient = models.PositiveIntegerField(null=False)
+    notification_date = models.DateTimeField(null=False, auto_now=True)
