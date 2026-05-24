235f2ca166f174d7c666ba72843dcd82b53382bd
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-05-24 15:43:42 +0200

    Creating views for the notifications' models

--- newsletter/notifications/views.py
+++ newsletter/notifications/views.py
@@ -8,3 +8,18 @@ class ResultPagination(PageNumberPagination):
     page_size = 25
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+
+class NotificationRunViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.NotificationRun.objects.all()
+    serializer_class = serializers.NotificationRunSerializer
+
+
+class EmailViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.EmailLog.objects.all()
+    serializer_class = serializers.EmailLogSerializer
+    
+
+class TemplateViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.Template.objects.all()
+    serializer_class = serializers.TemplateSerializer
