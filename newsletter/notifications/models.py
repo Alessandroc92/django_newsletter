30a19f3fa96176dc0fbba0de87c2400162b3fe5e
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-05-24 15:32:15 +0200

    Create a EmailLog model to track which employees has received any given e-mail

--- newsletter/notifications/models.py
+++ newsletter/notifications/models.py
@@ -12,4 +12,21 @@ class Template(models.Model):
     recipient = models.EmailField(null=False)
     subject = models.TextField(null=False)
     body = models.TextField(null=False)
-    created_at = models.DateTimeField(null=False, auto_now=True)
+    
+
+class EmailLog(models.Model):
+    notification_run = models.ForeignKey(
+        NotificationRun,
+        on_delete=models.DO_NOTHING,
+        null=True,
+        related_name='notification_runs'
+        
+    )
+    notification_id = models.AutoField(primary_key=True)
+    template = models.ForeignKey(
+        Template,
+        on_delete=models.DO_NOTHING,
+        null=True,
+        related_name='templates'
+    )
+    sent_at = models.DateTimeField(null=False, auto_now=True)
