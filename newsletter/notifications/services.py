e2e9303986ea2f83bb3fb3fbee5818972202ea0f
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-05-24 16:01:04 +0200

    Create a template_bday_info to send custom email to colleagues of people celebrating their birthday

--- newsletter/notifications/services.py
+++ newsletter/notifications/services.py
@@ -30,3 +30,32 @@ def template_bday(employee: employees.models.Employee) -> models.Template:
         subject=subject,
         body=body,
         )
+    
+
+def template_bday_info(
+    employee: employees.models.Employee,
+    bday_employees: list[employees.models.Employee],
+    ) -> models.Template:
+
+    bday_employees_data = '\n'.join(
+        f'Name: {bde.first_name} {bde.last_name} - E-mail Address: {bde.email_address}' 
+        for bde in bday_employees
+        )
+    subject = f'There are some colleagues celebrating their birthday today!!!'
+    body = f'''
+        Hey {employee.first_name}!
+        
+        Today there are {len(bday_employees)} colleagues celebrating their birthday!
+        Don't forget to contact them all! Here's a handy list for you:
+        
+        {bday_employees_data}
+        
+        Thanks,
+        
+        Global HR
+    '''
+    return models.Template(
+        recipient=employee.email_address,
+        subject=subject,
+        body=body,
+        )
