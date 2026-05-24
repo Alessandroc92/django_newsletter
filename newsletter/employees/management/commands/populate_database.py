10b3eb9659ce22e107e005dd60521b7e0bada6db
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-05-24 14:24:26 +0200

    Random creation of employees for each branch and consecutive e-mail creation

--- newsletter/employees/management/commands/populate_database.py
+++ newsletter/employees/management/commands/populate_database.py
@@ -4,6 +4,10 @@ import random
 import datetime
 
 
+FIRST_NAMES = ('John','Sarah','Nassim','Faroq','Clara')
+LAST_NAMES = ('Smith','Drinkwater','Rogue','Taleb','Dent')
+
+
 class Command(BaseCommand):
     
     def handle(self, *args, **kwargs):
@@ -36,4 +40,22 @@ class Command(BaseCommand):
         
         Team.objects.bulk_create(teams)
         team_objects = Team.objects.all()
-    
+
+        employees = [
+            Employee(
+                first_name=random.choice(FIRST_NAMES),
+                last_name=random.choice(LAST_NAMES),
+                birthdate=datetime.date(
+                    year=random.randint(1965,2005),
+                    month=random.randint(1,12),
+                    day=random.randint(1,28)
+                ),
+                active=random.randint(0,1),
+                team=random.choice(team_objects)
+            )
+                for _ in range(1000)
+        ]
+        for employee in employees:
+            employee.team = random.choice(team_objects)
+            employee.email_address = f'{employee.first_name}_{employee.last_name}{random.randint(1000,9999)}@company.com'
+            employee.save()
