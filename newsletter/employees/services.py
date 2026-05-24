cacfe174f9b250c40387b8574d67725d624a77ec
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-05-24 15:13:43 +0200

    Create a function to retrieve employees whose birthday isn't today and add docstring to functions

--- newsletter/employees/services.py
+++ newsletter/employees/services.py
@@ -6,11 +6,44 @@ def employees_celebrating_bday(
     status: bool=True,
     avoid_team_type: str='Administrative',
     ) -> models.Employee:
+    """A function that returns a list of Employee objects whose birthday is today.
+
+    Args:
+        status (bool, optional): Employment status is either active or inactive. Defaults to True.
+        avoid_team_type (str, optional): Team that doesn't receive emails.
+        Defaults to 'Administrative'.
+
+    Returns:
+        models.Employee: A list of Employee objects.
+    """
     now = datetime.datetime.now()
     employees = models.Employee.objects.filter(
         birthdate__day=now.day, 
         birthdate__month=now.month,
         active=True).exclude(
             team__team_type=avoid_team_type
+        )
+    return employees
+
+
+def employees_not_celebrating_bday(
+    status: bool=True,
+    avoid_team_type: str = 'Administrative') -> models.Employee:
+    """A function that returns a list of Employee objects whose birthday IS NOT today.
+
+    Args:
+        status (bool, optional): Employment status is either active or inactive. Defaults to True.
+        avoid_team_type (str, optional): Team that doesn't receive emails.
+        Defaults to 'Administrative'.
+
+    Returns:
+        models.Employee: A list of Employee objects.
+    """
+    now = datetime.datetime.now()
+    employees = Employee.objects.filter(
+        active=True).exclude(
+            team__team_type=avoid_team_type,
+            birthdate__day=now.day,
+            birthdate__month=now.month,
         )
     return employees
