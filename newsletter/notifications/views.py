30f63b4ac38981bffd7deba53a3f26db7b2145ab
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-05-24 15:41:44 +0200

    Import of modules and creation of pagination class

--- newsletter/notifications/views.py
+++ newsletter/notifications/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+from . import models
+from . import serializers
 
-# Create your views here.
+
+class ResultPagination(PageNumberPagination):
+    page_size = 25
+    page_size_query_param = 'page_size'
+    max_page_size = 100
