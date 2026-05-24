e0d5c380f6c2aefa9894f92a2bb13b241548f9d4
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-05-24 15:26:19 +0200

    Create a Template model to track emails sent to employees

--- newsletter/notifications/models.py
+++ newsletter/notifications/models.py
@@ -5,3 +5,11 @@ class NotificationRun(models.Model):
     notification_run_id = models.AutoField(primary_key=True)
     n_recipient = models.PositiveIntegerField(null=False)
     notification_date = models.DateTimeField(null=False, auto_now=True)
+    
+    
+class Template(models.Model):
+    template_id = models.AutoField(primary_key=True)
+    recipient = models.EmailField(null=False)
+    subject = models.TextField(null=False)
+    body = models.TextField(null=False)
+    created_at = models.DateTimeField(null=False, auto_now=True)
