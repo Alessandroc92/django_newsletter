93e5b8df1328f94301fd07e557e8881bfabc5e2a
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-05-24 13:52:50 +0200

    Add django-tasks and django rest framework to the installed apps in settings.py

--- newsletter/settings.py
+++ newsletter/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_tasks',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
