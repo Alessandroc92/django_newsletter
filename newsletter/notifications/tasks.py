10254cb8dc68a2a48c7f75c17ad1995907ba1616
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-05-24 16:17:39 +0200

    Create a task to send mass emails to employees

---
+++ newsletter/notifications/tasks.py
@@ -0,0 +1,9 @@
+from django.core.mail import send_mass_mail
+from django.tasks import task
+from . import services
+
+
+@task()
+def send_happy_bday_emails(**kwargs):
+    emails = services.create_emails_from_templates(**kwargs)
+    send_mass_mail(emails, fail_silently=False)
